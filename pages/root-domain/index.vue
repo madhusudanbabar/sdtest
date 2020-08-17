e660b79a22c67d82e48cc88ba8c46acd24e065d2
Krypton <madhusudanbabar@gmail.com>
2020-08-17 14:18:35 +0530

    initial commit

---
+++ pages/root-domain/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>im index from root domain</h2>
+        <h1>
+            hello there
+        </h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/sub-domain/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>
+            hello there
+        </h1>
+        <h2>im index from sub domain</h2>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
